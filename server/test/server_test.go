c178277e3ebd5e68b1d9dbb547543ff85da3ab68
Natalie Frecka <natalie.frecka@accenture.com>
2019-09-18 10:30:51 -0500

    Tweaking the go test to be clearer about what it is doing.
    Signed-off-by: Robert Murdock <robert.f.murdock@accenture.com>

--- server/test/server_test.go
+++ server/test/server_test.go
@@ -10,19 +10,17 @@ import (
 import "duty-designator/server/src"
 
 func TestGetCandidatesHandler_ReturnsCandidateJson(t *testing.T) {
-	req, err := http.NewRequest("GET", "/candidates ", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(src.GetTaskAssignmentsHandler)
 
-	handler.ServeHTTP(responseRecorder, req)
+	handler.ServeHTTP(responseRecorder, nil)
 
-	var rows []src.Row
-	rows = append(rows, src.Row{Candidate:"bob", Task:"dishes"} )
+	var rows = []src.Row{
+		{Candidate: "bob", Task: "dishes"},
+	}
+
 	jsonBytes, _ := json.Marshal(rows)
+
 	if responseRecorder.Body.String() != string(jsonBytes) {
 		t.Fail()
 	}
