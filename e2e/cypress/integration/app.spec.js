9b5137d0aee58b0abf82176f5973fe2f141e1bbd
Jeb Sumeracki <jeb.sumeracki@accenture.com>
2019-10-01 16:20:24 -0500

    Rename spec list and use xmllint for cypress test failure detection
    Signed-off-by: Seamus Riordan <seamus.riordan@accenture.com>

--- e2e/cypress/integration/app.spec.js
+++ e2e/cypress/integration/app.spec.js
@@ -25,4 +25,21 @@ context('Actions', () => {
     //         .should('have.text', 'jim joe jon')
     // })
 
+    async function insertChore(chore) {
+        await fetch("http://localhost:8080/api/chore", {
+            method: "POST",
+            body: JSON.stringify(chore),
+            signal: undefined
+        });
+    }
+ 
+    it('when a new chore is posted, it shows up on the page',  () => {
+        const chore = {name: "Dastardly Dishes", id: uuid()};
+        insertChore(chore);
+        
+        cy.visit('http://localhost:8080')
+        cy.get(`.chore[choreId=${chore.id}]`)
+          .should('have.text', chore.name)
+    });
+
 });
