3657719f1f971d0066a62ee46e263727f3f2ef71
Seamus Riordan <seamus.riordan@accenture.com>
2019-10-03 09:36:48 -0500

    Removed commented tests from cypress spec tests
    Signed-off-by: Robert Murdock <robert.f.murdock@accenture.com>

--- e2e/cypress/integration/app.spec.js
+++ e2e/cypress/integration/app.spec.js
@@ -9,21 +9,16 @@ context('Actions', () => {
             signal: undefined
         });
     }
- 
+
     it('when a new candidate is posted, it shows up on the page',  () => {
         const candidate = {name: "Jimmy Cypress", id: uuid()};
         insertCandidate(candidate);
-        
+
         cy.visit('http://localhost:8080')
         cy.get(`.candidate[candidateId=${candidate.id}]`)
-          .should('have.text', candidate.name)
+            .should('have.text', candidate.name)
     });
 
-    // it('fail', () => {
-    //     cy.visit('http://localhost:8080');
-    //     cy.get(`.candidate[candidateId="7"]`)
-    //         .should('have.text', 'jim joe jon')
-    // })
 
     async function insertChore(chore) {
         await fetch("http://localhost:8080/api/chore", {
@@ -32,30 +27,15 @@ context('Actions', () => {
             signal: undefined
         });
     }
- 
+
     it('when a new chore is posted, it shows up on the page',  () => {
         const chore = {name: "Dastardly Dishes", id: uuid()};
         insertChore(chore);
-        
+
         cy.visit('http://localhost:8080')
         cy.get(`.chore[choreId=${chore.id}]`)
-          .should('have.text', chore.name)
+            .should('have.text', chore.name)
     });
 
-    // it('when adding new chore through modal, saving adds new chore',  () => {
-    //     const chore = {name: "Rootin' Tootin' Rag Cleanin'", id: uuid()};
-    //     insertChore(chore)
-    //
-    //     cy.visit('http://localhost:8080');
-    //     cy.get('.chore').should('have.length', 1)
-    //
-    //     cy.get('#add-chore-button').click();
-    //
-    //     cy.get('#chore-name').type(chore.name)
-    //     cy.get('#save-chore-button').click();
-    //
-    //     cy.get(`.chore[choreId=${chore.id}]`)
-    //         .should('have.text', chore.name)
-    // });
-
 });
+
