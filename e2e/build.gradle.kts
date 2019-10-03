e135c5437f949d4002ec7a53841df013e92f4e55
Robert Murdock <robert.f.murdock@accenture.com>
2019-10-03 18:55:01 -0400

    Added e2e gradle task, and a check that triggers it

---
+++ e2e/build.gradle.kts
@@ -0,0 +1,32 @@
+import com.moowork.gradle.node.yarn.YarnTask
+
+plugins {
+    id("com.github.node-gradle.node") version "1.3.0"
+}
+
+node {
+    version = "12.10.0"
+    npmVersion = "6.11.3"
+    yarnVersion = "1.17.3"
+    download = true
+}
+
+tasks {
+
+    val yarn by getting(YarnTask::class) {
+
+    }
+
+    val endToEnd by creating(YarnTask::class) {
+        dependsOn(yarn)
+
+        setEnvironment(mapOf("CI" to "true"))
+
+        args = listOf("e2e")
+    }
+
+    val check by creating {
+        dependsOn(endToEnd)
+    }
+
+}
