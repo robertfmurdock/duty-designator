92dcbb03ab710012ada239c2ff2575cde3823e7e
Robert Murdock <robert.f.murdock@accenture.com>
2019-10-03 20:49:52 -0400

    Putting in tiny sleeps to help ci work possibly.

--- e2e/cypress/integration/app.spec.js
+++ e2e/cypress/integration/app.spec.js
@@ -8,6 +8,7 @@ context('Actions', () => {
             body: JSON.stringify(candidate),
             signal: undefined
         });
+        cy.wait(25);
     }
 
     it('when a new candidate is posted, it shows up on the page',  () => {
@@ -26,13 +27,14 @@ context('Actions', () => {
             body: JSON.stringify(chore),
             signal: undefined
         });
+        cy.wait(25);
     }
 
     it('when a new chore is posted, it shows up on the page',  () => {
         const chore = {name: "Dastardly Dishes", id: uuid()};
         insertChore(chore);
 
-        cy.visit('http://localhost:8080')
+        cy.visit('http://localhost:8080');
         cy.get(`.chore[choreId=${chore.id}]`, { timeout: 2000})
             .should('have.text', chore.name)
     });
