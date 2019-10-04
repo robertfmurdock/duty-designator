293bc947ca20e0770be37ae0cf494db1de821192
NatalieFrecka <nfrecka@gmail.com>
2019-10-04 11:53:26 -0500

    added addChore function to add chore modal

--- client/src/dashboard/AddChoreModal.js
+++ client/src/dashboard/AddChoreModal.js
@@ -1,22 +1,16 @@
 import React from "react";
-import {
-    TextField,
-    Button,
-    Dialog,
-    DialogContent
-} from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import { Work } from '@material-ui/icons';
+import {IconButton, TextField, Button, Dialog, DialogContent, Typography} from "@material-ui/core";
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import IconButton from '@material-ui/core/IconButton';
+import {withStyles} from "@material-ui/core/styles";
+import {Work} from '@material-ui/icons';
 import CloseIcon from '@material-ui/icons/Close';
-import Typography from '@material-ui/core/Typography';
 
 export default class AddChoreModal extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            choreName: ''
+            name: "",
+            description: ""
         };
     }
 
@@ -29,23 +23,29 @@ export default class AddChoreModal extends React.Component {
             position: 'absolute',
             right: theme.spacing(1),
             top: theme.spacing(1),
-            color: theme.palette.grey[500],
+            color: theme.palette.text.primary,
         },
     });
 
-    updateState = (e) => {
-        this.setState({ choreName: e.target.value })
+    updateName = e => this.setState({name: e.target.value});
+
+    updateDescription = e => this.setState({description: e.target.value});
+
+    addChore = () => {
+        const {name, description} = this.state;
+        this.props.addChore(name, description);
     };
 
     render() {
         const DialogTitle = withStyles(this.styles)(props => {
-            const { children, classes, onClose } = props;
+            const {children, classes, onClose} = props;
             return (
                 <MuiDialogTitle disableTypography className={classes.root}>
                     <Typography variant="h6">{children}</Typography>
                     {onClose ? (
-                        <IconButton id={"closeModalButton"} aria-label="close" className={classes.closeButton} onClick={onClose}>
-                            <CloseIcon />
+                        <IconButton id={"closeModalButton"} aria-label="close" className={classes.closeButton}
+                                    onClick={onClose}>
+                            <CloseIcon/>
                         </IconButton>
                     ) : null}
                 </MuiDialogTitle>
@@ -53,39 +53,44 @@ export default class AddChoreModal extends React.Component {
         });
 
         return <Dialog className="add-chore-modal"
-            aria-labelledby="customized-dialog-title"
-            open={this.props.open}
-            onClose={this.props.onClose}>
+                       aria-labelledby="customized-dialog-title"
+                       open={this.props.open}
+                       onClose={this.props.onClose}>
             <DialogTitle id="customized-dialog-title" onClose={this.props.onClose}>
                 <Work/>
             </DialogTitle>
+
             <DialogContent>
                 <TextField
                     id="chore-name"
                     label="What is the name of Chore you want to add to the list?"
                     placeholder="Add Chore Here"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    onChange={this.updateState}
+                    InputLabelProps={{shrink: true}}
+                    onChange={this.updateName}
                     autoFocus
                     margin="dense"
                     fullWidth
                 />
+
                 <TextField
                     id="chore-description"
                     label="What is the description of Chore you want to add to the list?"
                     placeholder="Add Description Name"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
+                    InputLabelProps={{shrink: true}}
+                    onChange={this.updateDescription}
                     margin="dense"
                     fullWidth
                 />
-                <Button variant="contained" color="primary" id='save-chore-button' disabled={!this.state.choreName}>
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    id='save-chore-button'
+                    disabled={!this.state.name}
+                    onClick={this.addChore}>
                     Update Chore List
                 </Button>
             </DialogContent>
-        </Dialog>
+        </Dialog>;
     }
 }
--- client/src/dashboard/AddChoreModal.test.js
+++ client/src/dashboard/AddChoreModal.test.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import AddChoreModal from './AddChoreModal.js'
+import {Button} from "@material-ui/core";
 
 describe('AddChoreModal', () => {
     let dialog;
+
     beforeEach(() => {
         dialog = shallow(<AddChoreModal/>);
     });
-    test('save button disabled with no data', () => {
 
-        expect(dialog.find('#save-chore-button').props().disabled).toBeTruthy()
+    test('save button disabled with no data', () => {
+        expect(dialog.find(Button).props().disabled).toBeTruthy()
     });
 
     test('save button enabled with chore name', () => {
-
-        dialog.find('#chore-name').simulate('change', {target: {value: 'Fake Chore Name'}});
-
-        expect(dialog.find('#save-chore-button').props().disabled).toBeFalsy()
+        simulateTyping('#chore-name', 'Fake Chore Name');
+        expect(dialog.find(Button).props().disabled).toBeFalsy()
     });
+
+    test('save button enabled with chore name', () => {
+        simulateTyping('#chore-name', 'Fake Chore Name');
+        expect(dialog.find(Button).props().disabled).toBeFalsy()
+    });
+
+    test('save button does the things', () => {
+        let actualChoreVals = {};
+        const addChoreSpy = (name, description) => actualChoreVals = {name, description};
+        dialog.setProps({addChore: addChoreSpy});
+
+        const expectedName = simulateTyping('#chore-name', 'Fake Chore Name');
+        const expectedDescription = simulateTyping('#chore-description', 'Fake Chore Description');
+
+        dialog.find(Button).simulate("click");
+
+        expect(actualChoreVals.name).toBe(expectedName);
+        expect(actualChoreVals.description).toBe(expectedDescription);
+    });
+
+    const simulateTyping = (id, value) => {
+        dialog.find(id).simulate('change', {target: {value}});
+        return value;
+    }
 });
