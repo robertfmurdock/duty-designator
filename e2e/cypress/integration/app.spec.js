87d5acb36df7c2404156d927514872bc3f23488d
Robert Murdock <robert.f.murdock@accenture.com>
2019-10-23 14:24:17 -0400

    Adding extremely small sleeps bc we're seeing inconsistent behavior in circle, and that could be related to db latency.

--- e2e/cypress/integration/app.spec.js
+++ e2e/cypress/integration/app.spec.js
@@ -14,17 +14,18 @@ context('Actions', () => {
 
         const candidate = {name: "Jimmy Cypress", id: uuid()};
 
-        beforeEach(async () => {
+        beforeEach(async function () {
             await insertCandidate(candidate);
-        })
+        });
+
+        beforeEach(() => cy.wait(40));
 
         it('it shows up on the page', () => {
-
             cy.visit('http://localhost:8080');
             cy.get(`.candidate[data-candidate-id=${candidate.id}]`, {timeout: 2000})
                 .should('have.text', candidate.name);
         });
-    })
+    });
 
     async function insertChore(chore) {
         await fetch("http://localhost:8080/api/chore", {
@@ -40,7 +41,9 @@ context('Actions', () => {
 
         beforeEach(async () => {
             await insertChore(chore);
-        })
+        });
+
+        beforeEach(() => cy.wait(40));
 
         it('it shows up on the page', () => {
             cy.visit('http://localhost:8080');
