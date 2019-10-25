863b6479248c2c28fc0bc88df0a060644b417bbc
Robert Murdock <robert.f.murdock@accenture.com>
2019-10-25 09:40:01 -0400

    Removing unused state from Results.js

--- client/src/results/Results.js
+++ client/src/results/Results.js
@@ -3,14 +3,6 @@ import {Container} from "@material-ui/core";
 import DutyTable from "../tables/DutyTable";
 
 export default class Results extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pioneers: [],
-            chores: [],
-            duties: props.duties
-        }
-    }
 
     render = () => (
         <Container className="results">
