7c80baeda6b45f589b80f9a5b3e16e8e0b8c93e3
Robert Murdock <robert.f.murdock@accenture.com>
2019-10-25 12:54:50 -0400

    Refactored the AddChoreModal into a functional component.

--- client/src/dashboard/AddChoreModal.js
+++ client/src/dashboard/AddChoreModal.js
@@ -1,96 +1,79 @@
-import React from "react";
-import {IconButton, TextField, Button, Dialog, DialogContent, Typography} from "@material-ui/core";
+import React, {useState} from "react";
+import {Button, Dialog, DialogContent, IconButton, TextField, Typography} from "@material-ui/core";
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import {withStyles} from "@material-ui/core/styles";
 import {Work} from '@material-ui/icons';
 import CloseIcon from '@material-ui/icons/Close';
 
-export default class AddChoreModal extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "",
-            description: ""
-        };
-    }
+const styles = theme => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(2),
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.text.primary,
+    },
+});
 
-    styles = theme => ({
-        root: {
-            margin: 0,
-            padding: theme.spacing(2),
-        },
-        closeButton: {
-            position: 'absolute',
-            right: theme.spacing(1),
-            top: theme.spacing(1),
-            color: theme.palette.text.primary,
-        },
-    });
+const DialogTitle = withStyles(styles)(props => {
+    const {children, classes, onClose} = props;
+    return (
+        <MuiDialogTitle disableTypography className={classes.root}>
+            <Typography variant="h6">{children}</Typography>
+            {onClose ? (
+                <IconButton id={"closeModalButton"} aria-label="close" className={classes.closeButton}
+                            onClick={onClose}>
+                    <CloseIcon/>
+                </IconButton>
+            ) : null}
+        </MuiDialogTitle>
+    );
+});
 
-    updateName = e => this.setState({name: e.target.value});
+export default function AddChoreModal(props) {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    return <Dialog className="add-chore-modal"
+                   aria-labelledby="customized-dialog-title"
+                   open={props.open}
+                   onClose={props.onClose}>
+        <DialogTitle id="customized-dialog-title" onClose={props.onClose}>
+            <Work/>
+        </DialogTitle>
 
-    updateDescription = e => this.setState({description: e.target.value});
+        <DialogContent>
+            <TextField
+                id="chore-name"
+                label="What is the name of Chore you want to add to the list?"
+                placeholder="Add Chore Here"
+                InputLabelProps={{shrink: true}}
+                onChange={e => setName(e.target.value)}
+                autoFocus
+                margin="dense"
+                fullWidth
+            />
 
-    addChore = () => {
-        const {name, description} = this.state;
-        this.props.addChore(name, description);
-    };
+            <TextField
+                id="chore-description"
+                label="What is the description of Chore you want to add to the list?"
+                placeholder="Add Description Name"
+                InputLabelProps={{shrink: true}}
+                onChange={e => setDescription(e.target.value)}
+                margin="dense"
+                fullWidth
+            />
 
-    render() {
-        const DialogTitle = withStyles(this.styles)(props => {
-            const {children, classes, onClose} = props;
-            return (
-                <MuiDialogTitle disableTypography className={classes.root}>
-                    <Typography variant="h6">{children}</Typography>
-                    {onClose ? (
-                        <IconButton id={"closeModalButton"} aria-label="close" className={classes.closeButton}
-                                    onClick={onClose}>
-                            <CloseIcon/>
-                        </IconButton>
-                    ) : null}
-                </MuiDialogTitle>
-            );
-        });
-
-        return <Dialog className="add-chore-modal"
-                       aria-labelledby="customized-dialog-title"
-                       open={this.props.open}
-                       onClose={this.props.onClose}>
-            <DialogTitle id="customized-dialog-title" onClose={this.props.onClose}>
-                <Work/>
-            </DialogTitle>
-
-            <DialogContent>
-                <TextField
-                    id="chore-name"
-                    label="What is the name of Chore you want to add to the list?"
-                    placeholder="Add Chore Here"
-                    InputLabelProps={{shrink: true}}
-                    onChange={this.updateName}
-                    autoFocus
-                    margin="dense"
-                    fullWidth
-                />
-
-                <TextField
-                    id="chore-description"
-                    label="What is the description of Chore you want to add to the list?"
-                    placeholder="Add Description Name"
-                    InputLabelProps={{shrink: true}}
-                    onChange={this.updateDescription}
-                    margin="dense"
-                    fullWidth
-                />
-
-                <Button
-                    variant="contained"
-                    color="primary"
-                    id='save-chore-button'
-                    disabled={!this.state.name}
-                    onClick={this.addChore}>
-                    Update Chore List
-                </Button>
-            </DialogContent>
-        </Dialog>;
-    }
+            <Button
+                variant="contained"
+                color="primary"
+                id='save-chore-button'
+                disabled={!name}
+                onClick={() => props.addChore(name, description)}>
+                Update Chore List
+            </Button>
+        </DialogContent>
+    </Dialog>;
 }
