b7227eab1e9aedac7f702c07c53c3959c86e149d
Robert Murdock <robert.f.murdock@accenture.com>
2019-10-25 09:51:04 -0400

    Refactored the ChoreTable component for clarity.

--- client/src/tables/ChoreTable.js
+++ client/src/tables/ChoreTable.js
@@ -10,41 +10,47 @@ const styles = {
 };
 
 export default function (props) {
+    const {chores, onRemove, addChoreHandler} = props;
     return <Box style={{padding: 16}} flex="0 0 400px">
-        <Typography className="table-title" variant="h5" color='textPrimary' align="center" gutterBottom>Today's CHORES</Typography>
+        <Typography className="table-title" variant="h5" color='textPrimary' align="center" gutterBottom>
+            Today's CHORES
+        </Typography>
         <Box border={2} borderColor="text.secondary">
             <Table>
                 <TableBody>
-                    {props.chores.map(row => (
-                        <TableRow key={row.id}>
-                            <TableCell className="chore" data-chore-id={row.id} style={styles.cell}>
-                                <Typography variant="h6" color='textPrimary'>
-                                    {row.name}
-                                </Typography>
-                            </TableCell>
-                            <TableCell align="right" style={styles.cell}>
-                                <Icon path={mdiClose} size={1} onClick={() => props.onRemove(row)}/>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                    <TableRow>
-                        <TableCell style={styles.cell}>
-                            <button
-                                id="add-chore-button"
-                                onClick={props.addChoreHandler}
-                            >
-                                <Typography variant="h6" color='textPrimary'>
-                                    <strong>Add new Chore to the list</strong>
-                                </Typography>
-                            </button>
-                        </TableCell>
-
-                        <TableCell align="right" style={styles.cell}>
-                            <Icon path={mdiPlus} size={1} color="primary.main" onClick={props.addChoreHandler}/>
-                        </TableCell>
-                    </TableRow>
+                    {chores.map(chore => (choreRow(chore, onRemove)))}
+                    {addChoreRow(addChoreHandler)}
                 </TableBody>
             </Table>
         </Box>
     </Box>
+}
+
+function choreRow(chore, onRemove) {
+    return <TableRow key={chore.id}>
+        <TableCell className="chore" data-chore-id={chore.id} style={styles.cell}>
+            <Typography variant="h6" color='textPrimary'>
+                {chore.name}
+            </Typography>
+        </TableCell>
+        <TableCell align="right" style={styles.cell}>
+            <Icon path={mdiClose} size={1} onClick={() => onRemove(chore)}/>
+        </TableCell>
+    </TableRow>;
+}
+
+function addChoreRow(addChoreHandler) {
+    return <TableRow>
+        <TableCell style={styles.cell}>
+            <button id="add-chore-button" onClick={addChoreHandler}>
+                <Typography variant="h6" color='textPrimary'>
+                    <strong>Add new Chore to the list</strong>
+                </Typography>
+            </button>
+        </TableCell>
+
+        <TableCell align="right" style={styles.cell}>
+            <Icon path={mdiPlus} size={1} color="primary.main" onClick={addChoreHandler}/>
+        </TableCell>
+    </TableRow>;
 }
