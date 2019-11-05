c136f7d1ea12329fe4a12e28da6a5c2d326c2e5e
Seamus Riordan <seamus.riordan@accenture.com>
2019-11-05 15:28:34 -0600

    [Bad-21] Move previous tests for ChoreHistoryTable
    Signed-off-by: Natalie Frecka <natalie.frecka@accenture.com>

--- client/src/chorehistory/ChoreHistoryTable.test.js
+++ client/src/chorehistory/ChoreHistoryTable.test.js
@@ -4,9 +4,30 @@ import ChoreHistoryTable from "./ChoreHistoryTable";
 
 describe('ChoreHistoryTable', () => {
     it('renders without crashing', () => {
-        let pioneer = {name: "Davey Doo", id: "does duties"}
+        let pioneer = {name: "Davey Doo", id: "does duties"};
+        let table = shallow(<ChoreHistoryTable pioneer={pioneer} choreCounts={[]}/>);
 
-        let table = shallow(<ChoreHistoryTable pioneer={pioneer} choreCounts={[]}/>)
-        expect(table.find('.pioneer-name').length).toEqual(1)
-    })
+        expect(table.find('.pioneer-name').length).toEqual(1);
+    });
+
+    it('renders pioneer name', () => {
+        let pioneer = {name: "Davey Doo", id: "does duties"};
+        let table = shallow(<ChoreHistoryTable pioneer={pioneer} choreCounts={[]}/>);
+
+        expect(table.find('.pioneer-name').text()).toBe(pioneer.name);
+    });
+
+    it('renders chore count rows for each chore count element', () => {
+        let pioneer = {name: "Davey Doo", id: "does duties"};
+        const dishes = {name: "Those Dish Things", id: "42", count: "1"};
+        const dj = {name: "Saloon DJ", id: "99", count: "4"};
+        let choreCounts = [dishes, dj];
+
+        let table = shallow(<ChoreHistoryTable pioneer={pioneer} choreCounts={choreCounts}/>);
+
+        expect(table.find('.chore-name').at(0).text()).toBe(dishes.name);
+        expect(table.find('.chore-count').at(0).text()).toBe(dishes.count);
+        expect(table.find('.chore-name').at(1).text()).toBe(dj.name);
+        expect(table.find('.chore-count').at(1).text()).toBe(dj.count);
+    });
 });
