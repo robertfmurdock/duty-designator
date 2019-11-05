43bbeb1a5d74538b4fa18e85631daca53250f19e
Seamus Riordan <seamus.riordan@accenture.com>
2019-11-05 14:00:43 -0600

    table displays chore counts
    Signed-off-by: Natalie Frecka <natalie.frecka@accenture.com>

--- client/src/chorehistory/ChoreHistory.js
+++ client/src/chorehistory/ChoreHistory.js
@@ -1,38 +1,31 @@
 import React, {useState} from 'react';
-import {Typography} from "@material-ui/core";
+import {Box, Table, TableBody, TableCell, TableRow, Typography} from "@material-ui/core";
 import FetchService from "../utilities/services/fetchService";
 import {loadStuff} from "../utilities/services/localStorageService";
 
 export default function ChoreHistory(props) {
     const [dataLoaded, setDataLoaded] = useState(false);
     const [pioneer, setPioneer] = useState(undefined);
+    const [choreCounts, setChoreCounts] = useState([]);
 
     if (!dataLoaded) {
-        setPioneerFromServer(props.id, setPioneer);
-
-        const dutyWithMatchedPioneerId = findPioneerDutyInLocalStorage(props.id);
-        if (dutyWithMatchedPioneerId) {
-            setPioneer(dutyWithMatchedPioneerId.pioneer)
-        }
-
-        setDataLoaded(true);
+        setPioneerFromServer(props.id, pioneer, setPioneer);
+        findPioneerDutyInLocalStorage(props.id, setPioneer, setChoreCounts);
+        setDataLoaded(true); // will need to be moved to take async fetch into account
     }
+
+    // console.log(pioneer);
+    // console.log(choreCounts);
 
     return <div>
         <Typography variant="h1" className="history-header">Pioneer History</Typography>
-        {conditionalRender(pioneer)}
+        {conditionalRender(pioneer, choreCounts)}
     </div>;
 }
 
-const conditionalRender = pioneer => (
+const conditionalRender = (pioneer, chores) => (
     pioneer
-        ? <Typography
-            variant="body1"
-            color="textPrimary"
-            className="pioneer-name"
-        >
-            {pioneer.name}
-        </Typography>
+        ? choreList(pioneer, chores)
         : <Typography
             variant="body1"
             color="textPrimary"
@@ -42,21 +35,74 @@ const conditionalRender = pioneer => (
         </Typography>
 );
 
+const choreList = (pioneer, choreCounts) => (
+    <Box style={{padding: 16}} flex="0 0 400px">
+        <Typography className="pioneer-name" variant="h5" color='textPrimary' align="center" gutterBottom>
+            {pioneer.name}
+        </Typography>
+        <Box border={2} borderColor="text.secondary">
+            <Table>
+                <TableBody>
+                    {choreCounts.map(chore => choreRow(chore))}
+                </TableBody>
+            </Table>
+        </Box>
+    </Box>
+);
+
+function choreRow(choreCount) {
+    return <TableRow key={choreCount.id}>
+        <TableCell className="chore" data-chore-id={choreCount.id}>
+            <Typography variant="h6" color='textPrimary' className="chore-name">
+                {choreCount.name}
+            </Typography>
+        </TableCell>
+        <TableCell align="right">
+            <Typography variant="h6" color='textPrimary' className="chore-count">
+                {choreCount.count}
+            </Typography>
+        </TableCell>
+    </TableRow>;
+}
+
 function flattenList(rosters) {
     return [].concat.apply([], rosters);
 }
 
-function findPioneerDutyInLocalStorage(id) {
+function findPioneerDutyInLocalStorage(id, setPioneer, setChoreCounts) {
     let rosters = Object.keys(localStorage)
         .map(date => loadStuff(date).dutyRoster)
         .filter(roster => roster !== false);
 
-    return flattenList(rosters)
-        .find(duty => duty.pioneer.id === id);
+    let duties = flattenList(rosters);
+
+    const dutyWithMatchedPioneerId = duties.find(duty => duty.pioneer.id === id);
+
+    if (dutyWithMatchedPioneerId) {
+        let choresWithCounts = [];
+        duties.forEach(duty => {
+            const chore = duty.chore;
+            const count = choresWithCounts.find(choreCount => choreCount.id === chore.id);
+
+            if(count) {
+                count.count++;
+            } else {
+                choresWithCounts.push({id: chore.id, name: chore.name, count: 1});
+            }
+        });
+
+        choresWithCounts.sort((a,b) => a.count - b.count);
+        setPioneer(dutyWithMatchedPioneerId.pioneer);
+        setChoreCounts(choresWithCounts)
+    }
 }
 
-function setPioneerFromServer(id, setPioneer) {
+function setPioneerFromServer(id, currentPioneer, setPioneer) {
     FetchService.post(0, "/api/pioneerById", {id}, undefined)
-        .then(pioneer => setPioneer(pioneer))
+        .then(pioneer => {
+            if(!currentPioneer) {
+                setPioneer(pioneer);
+            }
+        })
         .catch(err => console.warn("Problem fetching pioneer", err));
 }
--- client/src/chorehistory/ChoreHistory.test.js
+++ client/src/chorehistory/ChoreHistory.test.js
@@ -49,7 +49,7 @@ describe('ChoreHistory', () => {
 
     it('can get single pioneer from local storage with arbitrary date', () => {
         const pioneer = {name: "Juan Bonfante", id: "4"};
-        localStorage.setItem('10/29/2019', JSON.stringify({dutyRoster: [{pioneer: {name: "Guy", id: "7"}}]}));
+        localStorage.setItem('10/29/2019', JSON.stringify({dutyRoster: [{pioneer: {name: "Guy", id: "7"}, chore: {}}]}));
         localStorage.setItem('11/01/2019', JSON.stringify({dutyRoster: false}));
         localStorage.setItem('11/02/2019', JSON.stringify({dutyRoster: [{pioneer, chore: {}}]}));
 
@@ -62,6 +62,37 @@ describe('ChoreHistory', () => {
         let choreHistory = shallow(<ChoreHistory id="7"/>);
         expect(choreHistory.find('.pioneer-name').length).toEqual(0);
         expect(choreHistory.find('.no-pioneer').length).toEqual(1);
+    });
+
+    it('should not show chore rows if no chores are found', async () => {
+        const id = 3;
+        const name = "Greg";
+
+        fetchMock.mockReturnValue(
+            wrapInPromise({id, name})
+        );
+
+        let choreHistory = shallow(<ChoreHistory id={id}/>);
+        await waitUntil(() => choreHistory.find('.pioneer-name').length !== 0);
+        expect(choreHistory.find('.pioneer-name').text()).toBe(name)
+        expect(choreHistory.find('.chore-name').length).toEqual(0);
+        expect(choreHistory.find('.chore-description').length).toEqual(0);
+    });
+
+    it('will list users chore history ', async () => {
+        const pioneer = {name: "Juan Bonfante", id: "4"};
+        const chore1 = {name: "Cable Wrangler", id: "74"};
+        const chore2 = {name: "Saloon DJ", id: "8"};
+        localStorage.setItem('10/29/2019', JSON.stringify({dutyRoster: [{pioneer, chore: chore1}, {pioneer, chore: chore2}]}));
+        localStorage.setItem('11/02/2019', JSON.stringify({dutyRoster: [{pioneer, chore: chore2}]}));
+
+        let choreHistory = shallow(<ChoreHistory id={pioneer.id}/>);
+        await waitUntil(() => choreHistory.find('.chore-name').length !== 0);
+
+        expect(choreHistory.find('.chore-name').at(0).text()).toBe(chore1.name);
+        expect(choreHistory.find('.chore-count').at(0).text()).toBe("1");
+        expect(choreHistory.find('.chore-name').at(1).text()).toBe(chore2.name);
+        expect(choreHistory.find('.chore-count').at(1).text()).toBe("2");
     });
 
     const wrapInPromise = response => new Promise(resolve => resolve(response));
