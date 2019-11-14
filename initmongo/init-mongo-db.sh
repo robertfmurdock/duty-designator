536afff239f8f4514b7ffb8100b5060617ac3876
Seamus Riordan <seamus.riordan@accenture.com>
2019-11-14 12:52:22 -0600

    [Bad-57] Docker and mongo initialization scripts
    Signed-off-by: Natalie Frecka <natalie.frecka@accenture.com>

--- initmongo/init-mongo-db.sh
+++ initmongo/init-mongo-db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mongod --fork --logpath mongodb.log
-mongo drop-dutyDB.js
+mongod --fork --logpath /mongodb.log
+#mongo localhost/dutyDB dropdb.js
+mongo localhost/dutyDB drop_chores_and_pioneers.js
 mongoimport --type=csv --file=chores.csv --headerline -ddutyDB -cchores
 mongoimport --type=csv --file=pioneers.csv --headerline -ddutyDB -cpioneers
 mongo fsync.js
